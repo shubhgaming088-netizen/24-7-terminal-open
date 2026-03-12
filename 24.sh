26e9aedfa12ba3c43b368cbcf0a0245cde663778
shubhgaming088-netizen <shubhgaming088@gmail.com>
2026-03-12 08:21:02 +0530

    Create 24.sh

---
+++ 24.sh
@@ -0,0 +1,11 @@
+apt update 
+apt install screen -y
+screen
+screen -S terminal
+#!/usr/bin/env bash
+# ==========================================================
+# Run That Script That You Want | Then Press Ctrl -
++ A And then D
+# DATE: 2026-03-07 | UI-TYPE: SEMA-HYPER-VISUAL
+# ==========================================================
+set -euo pipefail
